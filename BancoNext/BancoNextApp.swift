3fe6baded46fbc1263bc48e87dc241d2420c2d82
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-12-28 15:59:08 -0300

    Initial Commit

---
+++ BancoNext/BancoNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  BancoNextApp.swift
+//  BancoNext
+//
+//  Created by Raphael Cerqueira on 28/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BancoNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BancoNext/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BancoNext
+//
+//  Created by Raphael Cerqueira on 28/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
